dce138e92b09c3641a93cea3041f04080c990985
Satvik Prasad <satvik.prsd@gmail.com>
2025-04-14 17:10:02 +0530

    changes for render

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 const corsOptions = {
-    origin: [process.env.URL],
+    origin: [process.env.URL,'https://notinsta.vercel.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 }
